39ca429150c5a518c49593a9ec8c870efd2bb922
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2022-12-10 22:41:15 +0600

    user - add

--- user/forms.py
+++ user/forms.py
@@ -6,13 +6,18 @@ from user.models import User
 class NewUserForm(forms.ModelForm):
     first_name = forms.CharField(widget=forms.TextInput(attrs={
         'placeholder': 'Enter user first name',
+        'class': 'form-control',
     }), required=True)
     last_name = forms.CharField(widget=forms.TextInput(attrs={
         'placeholder': 'Enter user first name',
+        'class': 'form-control',
     }), required=True)
     img = forms.FileField(widget=forms.ClearableFileInput(attrs={
+        'class': 'form-control',
     }), required=False)
     address = forms.CharField(widget=forms.Textarea(attrs={
+        'class': 'form-control',
+        'rows': '5',
     }), required=False)
 
     class Meta:
