b21e76a8130077f995ba54d78c6bc978407059d5
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2022-12-10 23:00:52 +0600

    payment - model

--- order_custom/admin.py
+++ order_custom/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 from order_custom.models import Purchase
+from order_custom.models import Payment
 
 
 admin.site.register(Purchase)
+admin.site.register(Payment)
--- order_custom/models.py
+++ order_custom/models.py
@@ -8,4 +8,7 @@ class Purchase(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
-# Payment
+
+class Payment(models.Model):
+    purchase = models.ForeignKey(Purchase, on_delete=models.CASCADE)
+    amount = models.DecimalField(default=0.0, max_digits=10, decimal_places=2)
